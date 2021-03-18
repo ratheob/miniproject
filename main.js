529b52aa0388e1830e74bd2e0509c265fbe535d9
ratheob <a12040898@unet.univie.ac.at>
2021-03-18 16:40:06 +0100

    nn

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function outputData(person) {
 
   // Add a class to age, shown everyone born 1950 or earlier*
   if ((new RegExp(/19[0-5]0/gi)).test(person.birthyear)) {
-    row.classList.add('auto');
+    row.classList.add('age');
 }
 }
 
