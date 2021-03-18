58289633d2c8f38af1a0d90907df6d8f7ff0c0a1
ratheob <a12040898@unet.univie.ac.at>
2021-03-18 16:35:52 +0100

    nn

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function outputData(person) {
   // Add a class to age, shown everyone born 1950 or earlier*
   if ((new RegExp(/19[0-5]*/gi)).test(person.birthyear)) {
     row.classList.add('auto');
-
+}
 }
 
 // The below function will get called when the window finishes loading our data
